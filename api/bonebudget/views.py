4c1bee83b49b0591d2b442c916461c19449b43fc
brukeg <bruke@spreadlabel.com>
2023-07-06 20:47:23 -0700

    Adds data models

--- api/bonebudget/views.py
+++ api/bonebudget/views.py
@@ -10,3 +10,27 @@ def index(request):
 def colors(request):
     colors = Colors.objects.all()
     return JsonResponse(list(colors.values()), safe=False)
+
+# def index(request):
+#     user_obj = User_entity.objects.all()
+#     context = {'users': user_obj}
+#     return render(request, 'home/index.html', context=context)
+
+
+# def upload(request):
+#     """
+#     Get the sign, transaction timestamp, description, transaction type, and balance
+#     from an uploaded CSV.
+#     """
+#     for transaction in csv:
+#         """
+# 		transaction_name = some_csv_attribute.name
+# 		trans   action_category = some_csv_attribute.category
+# 		transaction_type = some_csv_attribute.type
+# 		if transaction_type == 'debit':
+# 			transaction_amount = some_csv_attribute.amount * -1.0
+# 		# write transactions to db
+# 		return transaction
+#         """"
+#
+#     return render(request, '/upload.html', {})
