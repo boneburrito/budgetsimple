2af04329a53d9e64dbd61c6c1977d3f7ff457660
Amy Boyle <aboyle5@hotmail.com>
2023-07-15 16:42:09 +0000

    Switch to user model based on Django model
    Gives us authentication for free. Official recommended way to set
    up user model.

--- api/bonebudget/models.py
+++ api/bonebudget/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 import uuid
 
 # Create your models here.
@@ -15,16 +17,8 @@ class Colors(models.Model):
     def __str__(self):
         return self.name
 
-
-class User(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    email = models.EmailField(max_length=255)
-    password = models.CharField(max_length=100)
-    first_name =  models.CharField(max_length=100)
-    last_name=models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
+class User(AbstractUser):
+    pass
 
 class Transaction(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
